cdc625ab4737957f779240b1677756acb7145ca5
Opa Maj <saintkilla79@gmail.com>
2025-04-05 13:19:28 +0300

    Data access enitites

---
+++ GhostVillages.DataAccess/Entities/RegionEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostVillages.DataAccess.Entities
+{
+    public class RegionEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public byte[] Image { get; set; } = null!;
+
+        public virtual ICollection<VillageEntity> Villages { get; set; } = [];
+    }
+}
---
+++ GhostVillages.DataAccess/Entities/VillageEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostVillages.DataAccess.Entities
+{
+    public class VillageEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public byte[] Image { get; set; } = null!;
+
+        public Guid RegionId { get; set; }
+
+        public virtual RegionEntity Region { get; set; } = null!;
+    }
+}
