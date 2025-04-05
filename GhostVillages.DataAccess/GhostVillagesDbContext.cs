fa152088484277953862469dfc70a00b624c93c9
Opa Maj <saintkilla79@gmail.com>
2025-04-05 13:27:02 +0300

    Data access DbContext

---
+++ GhostVillages.DataAccess/GhostVillagesDbContext.cs
@@ -0,0 +1,23 @@
+﻿using GhostVillages.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostVillages.DataAccess
+{
+    public class GhostVillagesDbContext : DbContext
+    {
+        public GhostVillagesDbContext(DbContextOptions<GhostVillagesDbContext> options)
+            : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        public DbSet<VillageEntity> Villages { get; set; }
+
+        public DbSet<RegionEntity> Regions { get; set; }
+    }
+}
